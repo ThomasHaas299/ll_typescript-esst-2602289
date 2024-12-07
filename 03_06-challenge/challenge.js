fa047e32492e150970ad9aac1f2f19c3024dff31
Thomas Haas <github@thomashaas.de>
2024-12-07 13:00:15 +0000

    03_06

---
+++ 03_06-challenge/challenge.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+exports.Challenge = void 0;
+function Challenge() {
+    // Lösung mit Generic
+    // type MyType = string | false | Array<(number | string)>
+    var foo = "Hello!";
+    foo = false;
+    foo = ["test", 100];
+}
+exports.Challenge = Challenge;
--- 03_06-challenge/challenge.ts
+++ 03_06-challenge/challenge.ts
@@ -1,4 +1,11 @@
 export function Challenge() {
+
+  // Lösung mit Generic
+  // type MyType = string | false | Array<(number | string)>
+  
+  // Lösung ohne Generic
+  type MyType = string | false | (number | string)[]
+
   let foo: MyType = "Hello!";
   foo = false;
   foo = ["test", 100];
